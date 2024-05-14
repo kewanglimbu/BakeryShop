a132d5b842454bb3c8546a3fb19115aebc461ab1
kewang <kewang.h.limbu@gmail.com>
2024-05-14 17:34:43 +0545

    feat: add custom configuration for Customer entity using Fluent API

---
+++ BakeryShop/Data/BakeryShopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BakeryShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BakeryShop.Data
+{
+    public class BakeryShopDbContext: DbContext
+    {
+        public BakeryShopDbContext(DbContextOptions<BakeryShopDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<BakeryItem> BakeryItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BakeryShopDbContext).Assembly);
+        }
+    }
+}
---
+++ BakeryShop/Data/Configuration/CustomerEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BakeryShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BakeryShop.Data.Configuration
+{
+    public class CustomerEntityTypeConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder
+                .Property(p => p.FirstName).IsRequired();
+            builder
+                .Property(p => p.LastName).IsRequired();
+        }
+    }
+}
